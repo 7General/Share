ede4dc5bb6c87866d865748920609c861a094cc7
王会洲 <wanghuizhou21@163.com>
2016-04-21 14:09:05 +0800

    上传基本工程

---
+++ Share/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Share
+//
+//  Created by 王会洲 on 16/4/21.
+//  Copyright © 2016年 王会洲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
